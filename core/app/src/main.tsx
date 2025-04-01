34bd17f21776d291bbe8af65e37fa057fd73f2d3
Andrew Pepper-Anderson <apepperanderson@MacBook-Pro.local>
2025-03-31 23:18:21 -0400

    style import working

--- core/app/src/main.tsx
+++ core/app/src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@radix-ui/colors/orange.css"; // Force Vite to include it
+import "@radix-ui/colors/gray.css";
+import "@radix-ui/colors/green.css";
+import "@radix-ui/colors/yellow.css";
+import "@radix-ui/colors/red.css";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Link } from "react-router";
@@ -14,16 +19,20 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="dark">
+        <div className="background">
+          <BrowserRouter>
+            <Link to="/">Home</Link>
+            <Link to="/users">Users</Link>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
     </QueryClientProvider>
   </StrictMode>
 );
